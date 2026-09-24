47115e4e4b7ae09dc25442cbd3d3f2595883210d
imionic <das408265@gmail.com>
2026-09-24 20:34:01 +0530

    v1.2: Modularized code using functions

--- rental.py
+++ rental.py
@@ -1,34 +1,38 @@
 user_name = input("Enter your name: ")
 print("Hello, " + user_name + "! Welcome to RoadCo")
+car_type = " "
+def car_choose():
+    while True:
+        car_type = input("Enter the type of car you want to rent Sedan, SUV: ").strip().lower()
+        if car_type in ("sedan", "suv"):
+            return car_type.upper() if car_type == "suv" else car_type.title()
+        print("Please enter Sedan or SUV.")
 
-while True:
-    car_type = input("Enter the type of car you want to rent Sedan, SUV: ").strip().lower()
-    if car_type in ("sedan", "suv"):
-        car_type = car_type.upper() if car_type == "suv" else car_type.title()
-        break
-    print("Please enter Sedan or SUV.")
+choosen_car = car_choose()
+print("Car Type: " + choosen_car)
 
-print("Car Type: " + car_type)
+def rental_days():
+    while True:
+        try:
+            days = int(input("Enter the number of days you want to rent: "))
+            if days <= 0:
+                print("Please enter a valid number of days.")
+                continue
+            return days
+        except ValueError:
+            print("Please enter a whole number of days.")
 
-while True:
-    try:
-        days = int(input("Enter the number of days you want to rent: "))
-        print(f"Days rented: {days}")
-        if days <= 0:
-            print("Please enter a valid number of days.")
-            continue
-        break
-    except ValueError:
-        print("Please enter a whole number of days.")
+req_days = rental_days()
+print(f"Days Rented: {req_days}")
 
-if car_type == "Sedan":
+if choosen_car == "Sedan":
     rental_cost_per_day = 30
-elif car_type == "SUV":
+elif choosen_car == "SUV":
     rental_cost_per_day = 50
 
-total_cost = days * rental_cost_per_day
+total_cost = req_days * rental_cost_per_day
 
-if days > 7:
+if req_days > 7:
     discount = 10
     final_total_cost = total_cost * (1 - discount / 100)
     total_discount = total_cost - final_total_cost
@@ -38,4 +42,4 @@ else:
     final_total_cost = total_cost
     print(f"No discount applied. Sub Total: ${final_total_cost}")
 
-print(f"The total cost of renting the {car_type} for {days} days is: ${final_total_cost}")
+print(f"The total cost of renting the {choosen_car} for {req_days} days is: ${final_total_cost}")
